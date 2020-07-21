04418c93c11989bf359ac1359ab2ce05959b3b12
Carter Parrett <cparrett94@hotmail.com>
2020-07-21 02:37:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,7 +29,7 @@ class Owner
     Cat.all.select {|cat| cat.owner == self}
   end
   def dogs
-    dog.all.select {|dog| dog.owner == self}
+    Dog.all.select {|dog| dog.owner == self}
   end
   
   
