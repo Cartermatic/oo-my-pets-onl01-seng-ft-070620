d4c822252abdd126f8ae222df02b9ac154c90285
Carter Parrett <cparrett94@hotmail.com>
2020-07-21 02:26:42 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,13 @@ class Cat
   attr_accessor :owner
   attr_reader :name
   
-  @@all
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+  end
   
   
   
