9326db4a865dfae76708859c502920d5a03f87a1
Carter Parrett <cparrett94@hotmail.com>
2020-07-21 02:16:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,18 @@
 class Owner
-  attr_accessor :name
+  attr_accessor :name, :species
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
   
   
   
